4c4ca6957cd2c53c573e59739043b2b8f65522ac
Claudio <fanconic@student.ethz.ch>
2023-10-14 20:04:08 +0200

    Implemet BERT, GPT2, MPNet and RoBERTa

--- src/models/models.py
+++ src/models/models.py
@@ -11,11 +11,11 @@ from transformers import (
 from sentence_transformers import SentenceTransformer
 import torch.nn.functional as F
 import numpy as np
-
+import clip
 
 def ned_torch(x1, x2, dim=1, eps=1e-8):
     ned_2 = 0.5 * ((x1 - x2).var(dim=dim) / (x1.var(dim=dim) + x2.var(dim=dim) + eps))
-    return ned_2 ** 0.5
+    return ned_2**0.5
 
 
 def nes_torch(x1, x2, dim=1, eps=1e-8):
@@ -35,41 +35,95 @@ class MLP(nn.Module):
         embedded = self.embedding(text, offsets)
         return self.fc(embedded)
 
-
-class GPT2(nn.Module):
-    def __init__(self, vocab_size, model_configs):
-        super(GPT2, self).__init__()
-        # fine_tune = model_configs["fine_tune"]
-        num_class = model_configs["n_classes"]
-
-        self.gpt2_classifier = GPT2ForSequenceClassification.from_pretrained(
-            "gpt2", num_labels=num_class
-        )
-        self.gpt2_classifier.config.pad_token_id = 50256
-        if model_configs["freeze_layers"]:
-            for param in self.gpt2_classifier.base_model.parameters():
-                param.requires_grad = False
-
-    def forward(self, tokenized_text, attention_mask):
-        return self.gpt2_classifier(tokenized_text, attention_mask=attention_mask)
-
-
 class BERT(nn.Module):
     def __init__(self, vocab_size, model_configs):
         super(BERT, self).__init__()
         # fine_tune = model_configs["fine_tune"]
         num_class = model_configs["n_classes"]
 
-        self.bert_classifier = BertForSequenceClassification.from_pretrained(
-            "bert-base-uncased", num_labels=num_class
-        )
+        self.LM = SentenceTransformer("bert-large-nli-mean-tokens")
+        self.classifier = nn.Linear(model_configs["embed_dim"], model_configs["n_classes"])
+        
+        self.precomputed = model_configs["freeze_layers"]
+
         if model_configs["freeze_layers"]:
-            for param in self.bert_classifier.base_model.parameters():
+            for param in self.LM.parameters():
                 param.requires_grad = False
 
     def forward(self, tokenized_text, attention_mask):
-        # print(tokenized_text.shape)
-        return self.bert_classifier(tokenized_text)
+        if self.precomputed:
+            return self.classifier(tokenized_text)
+        else:
+            x = self.LM(tokenized_text)
+            return self.classifier(x)
+        
+class GPT2(nn.Module):
+    def __init__(self, vocab_size, model_configs):
+        super(GPT2, self).__init__()
+        # fine_tune = model_configs["fine_tune"]
+        num_class = model_configs["n_classes"]
+
+        self.LM = SentenceTransformer('Muennighoff/SGPT-125M-weightedmean-nli-bitfit')
+        self.LM.max_sequence_length = 2048
+        self.LM.tokenizer.pad_token = '[PAD]'
+        self.classifier = nn.Linear(model_configs["embed_dim"], num_class)
+        
+        self.precomputed = model_configs["freeze_layers"]
+
+        if model_configs["freeze_layers"]:
+            for param in self.LM.parameters():
+                param.requires_grad = False
+
+    def forward(self, tokenized_text, attention_mask):
+        if self.precomputed:
+            return self.classifier(tokenized_text)
+        else:
+            x = self.LM(tokenized_text)
+            return self.classifier(x)
+        
+class RoBERTa(nn.Module):
+    def __init__(self, vocab_size, model_configs):
+        super(RoBERTa, self).__init__()
+        # fine_tune = model_configs["fine_tune"]
+        num_class = model_configs["n_classes"]
+
+        self.LM = SentenceTransformer("sentence-transformers/all-distilroberta-v1")
+        self.classifier = nn.Linear(model_configs["embed_dim"], num_class)
+        
+        self.precomputed = model_configs["freeze_layers"]
+
+        if model_configs["freeze_layers"]:
+            for param in self.LM.parameters():
+                param.requires_grad = False
+
+    def forward(self, tokenized_text, attention_mask):
+        if self.precomputed:
+            return self.classifier(tokenized_text)
+        else:
+            x = self.LM(tokenized_text)
+            return self.classifier(x)
+        
+class MPNET(nn.Module):
+    def __init__(self, vocab_size, model_configs):
+        super(MPNET, self).__init__()
+        # fine_tune = model_configs["fine_tune"]
+        num_class = model_configs["n_classes"]
+
+        self.LM = SentenceTransformer("sentence-transformers/all-mpnet-base-v2")
+        self.classifier = nn.Linear(model_configs["embed_dim"], num_class)
+        
+        self.precomputed = model_configs["freeze_layers"]
+
+        if model_configs["freeze_layers"]:
+            for param in self.LM.parameters():
+                param.requires_grad = False
+
+    def forward(self, tokenized_text, attention_mask):
+        if self.precomputed:
+            return self.classifier(tokenized_text)
+        else:
+            x = self.LM(tokenized_text)
+            return self.classifier(x)
 
 
 class ProtoNet(nn.Module):
@@ -86,9 +140,15 @@ class ProtoNet(nn.Module):
             nn.init.uniform_(torch.empty(1, self.n_prototypes, self.dim), -1, 1),
             requires_grad=True,
         )
-        if self.metric in ["weighted_cosine", "weighted_L2"]:
+        if self.metric in ["weighted_cosine"]:
             self.dim_weights = nn.parameter.Parameter(
                 nn.init.ones_(torch.empty(self.dim)),
+                requires_grad=True,
+            )
+            
+        elif self.metric in ["weighted_L2"]:
+            self.dim_weights = nn.parameter.Parameter(
+                nn.init.zeros_(torch.empty(self.dim)),
                 requires_grad=True,
             )
 
@@ -142,7 +202,7 @@ class ProtoNet(nn.Module):
             )
         elif self.metric == "weighted_L2":
             prototype_distances = torch.cdist(
-                self.dim_weights * embedding.float(), self.protolayer.squeeze(), p=2
+                2*torch.sigmoid(self.dim_weights) * embedding.float(), self.protolayer.squeeze(), p=2
             ).squeeze(1) / np.sqrt(self.dim)
         elif self.metric == "dot_product":
             # exp(-x.T*y)
@@ -170,6 +230,7 @@ class ProtoNet(nn.Module):
             config["model"]["submodel"] == "bert"
             and config["model"]["embedding"] == "sentence"
         ):
+            print("saving embeddings")
             LM = SentenceTransformer("bert-large-nli-mean-tokens", device=device)
             labels = torch.empty((len(x)))
             embedding = torch.empty((len(x), config["model"]["embed_dim"]))
@@ -181,11 +242,12 @@ class ProtoNet(nn.Module):
                     .detach()
                 )
                 if idx % 100 == 0:
-                    print(idx)
+                    pass #print(idx)
         elif (
             config["model"]["submodel"] == "roberta"
             and config["model"]["embedding"] == "sentence"
         ):
+            print("saving embeddings")
             LM = SentenceTransformer(
                 "sentence-transformers/all-distilroberta-v1", device=device
             )
@@ -199,11 +261,12 @@ class ProtoNet(nn.Module):
                     .detach()
                 )
                 if idx % 100 == 0:
-                    print(idx)
+                    pass #print(idx)
         elif (
             config["model"]["submodel"] == "mpnet"
             and config["model"]["embedding"] == "sentence"
         ):
+            print("saving embeddings")
             LM = SentenceTransformer(
                 "sentence-transformers/all-mpnet-base-v2", device=device
             )
@@ -217,7 +280,56 @@ class ProtoNet(nn.Module):
                     .detach()
                 )
                 if idx % 100 == 0:
-                    print(idx)
+                    pass #print(idx)
+        elif config["model"]["submodel"] == 'gpt2':
+            print("saving embeddings")
+            LM = SentenceTransformer('Muennighoff/SGPT-125M-weightedmean-nli-bitfit', device=device)
+            LM.max_sequence_length = 2048
+            #LM.tokenizer.pad_token_id = 50256
+            LM.tokenizer.pad_token = '[PAD]'
+            labels = torch.empty((len(x)))
+            embedding = torch.empty((len(x), config["model"]["embed_dim"]))
+            for idx, (label, input) in enumerate(x):
+                labels[idx] = label
+                embedding[idx] = (
+                    LM.encode(input, convert_to_tensor=True, device=device)
+                    .cpu()
+                    .detach()
+                )
+                if idx % 100 == 0:
+                    pass #print(idx)
+                
+        elif config["model"]["submodel"] == 't5':
+            print("saving embeddings")
+            LM = SentenceTransformer('sentence-t5-xxl', device=device)
+            #LM.tokenizer.pad_token_id = 50256
+            LM.tokenizer.pad_token = '[PAD]'
+            labels = torch.empty((len(x)))
+            embedding = torch.empty((len(x), config["model"]["embed_dim"]))
+            for idx, (label, input) in enumerate(x):
+                labels[idx] = label
+                embedding[idx] = (
+                    LM.encode(input, convert_to_tensor=True, device=device)
+                    .cpu()
+                    .detach()
+                )
+                if idx % 100 == 0:
+                    pass #print(idx)
+                    
+        elif config["model"]["submodel"] == 'clip':
+            print("saving embeddings")
+            LM = SentenceTransformer('sentence-transformers/clip-ViT-B-32', device=device)
+            labels = torch.empty((len(x)))
+            embedding = torch.empty((len(x), config["model"]["embed_dim"]))
+            for idx, (label, input) in enumerate(x):
+                labels[idx] = label
+                embedding[idx] = (
+                    LM.encode(input, convert_to_tensor=True, device=device)
+                    .cpu()
+                    .detach()
+                )
+                if idx % 100 == 0:
+                    pass#print(idx)
         else:
             raise NotImplemented
 
@@ -226,7 +338,7 @@ class ProtoNet(nn.Module):
         if len(embedding.size()) == 1:
             embedding = embedding.unsqueeze(0).unsqueeze(0)
         mask = torch.ones(embedding.shape)  # required for attention models
-        return embedding, mask, (labels - 1)
+        return embedding, mask, (labels - 1).to(torch.long)
 
     @staticmethod
     def nearest_neighbors(distances, _, text_train, labels_train):
